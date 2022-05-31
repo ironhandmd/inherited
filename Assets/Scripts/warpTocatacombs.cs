fec10a35f155321c7880af5f45393b4558f8dcf0
ironhandmd <93461196+ironhandmd@users.noreply.github.com>
2022-05-31 15:12:05 +0800

    Add files via upload

---
+++ Assets/Scripts/warpTocatacombs.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class warpTocatacombs : MonoBehaviour
+{
+    [SerializeField]
+    Transform warpTarget;
+    camera_movement CM;
+
+    void Start()
+    {
+        CM = FindObjectOfType<camera_movement>();
+    }
+
+
+
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        CM.moveToArea4();
+        other.gameObject.transform.position = warpTarget.transform.position;
+    }
+
+
+}
---
+++ Assets/Scripts/warpbackToRoom3.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class warpbackToRoom3 : MonoBehaviour
+{
+
+    [SerializeField]
+    Transform warpTarget;
+    camera_movement CM;
+
+    void Start()
+    {
+        CM = FindObjectOfType<camera_movement>();
+    }
+
+
+
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        CM.moveToAreaManor3();
+        other.gameObject.transform.position = warpTarget.transform.position;
+    }
+
+
+
+
+
+}

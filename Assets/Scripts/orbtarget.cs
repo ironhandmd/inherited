5580a2993e918b0ad2629d1c02272441cd388055
ironhandmd <93461196+ironhandmd@users.noreply.github.com>
2022-05-31 15:12:51 +0800

    Add files via upload

---
+++ Assets/Scripts/orbtarget.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class orbtarget : MonoBehaviour
+{
+    scene_manager SceneManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        SceneManager = FindObjectOfType<scene_manager>();
+    }
+
+    public void orbHit() 
+    {
+        StartCoroutine(orbWasHit());
+    
+    }
+
+    IEnumerator orbWasHit() 
+    {
+        yield return new WaitForSeconds(2f);
+        SceneManager.LoadEnding1();
+    }
+
+}
